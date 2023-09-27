8bce1ee717bb0556efbf33ee79276097d9a0888b
Connor Richmond <richmond.connor@gmail.com>
2023-09-27 01:47:40 -0400

    test for errors

--- spec/requests/api/v0/login_request_spec.rb
+++ spec/requests/api/v0/login_request_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "User log in endpoint" do
-  before(:each) do
+
+  before do
     @user = User.create(email: "test@test.com", password: "test_password", password_confirmation: "test_password", api_key: "key")
   end
 
@@ -25,5 +26,50 @@ RSpec.describe "User log in endpoint" do
     expect(data[:attributes][:email]).to eq("test@test.com")
     expect(data[:attributes]).to have_key(:api_key)
     expect(data[:attributes][:api_key]).to eq("key")
+  end
+
+  it "gives error when wrong password" do
+
+    params = {
+      "email": "test@test.com",
+      "password": "bad_test_password"
+    }
+
+    post "/api/v0/sessions", params: params.to_json, headers: {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+    expect(response.status).to eq(401)
+    
+    message = JSON.parse(response.body, symbolize_names: true)
+    expect(message).to eq({message: "Your credentials are invalid"})
+  end
+
+  it "gives error when wrong email" do
+
+    params = {
+      "email": "badtest@test.com",
+      "password": "test_password"
+    }
+
+    post "/api/v0/sessions", params: params.to_json, headers: {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+    expect(response.status).to eq(401)
+    
+    message = JSON.parse(response.body, symbolize_names: true)
+    expect(message).to eq({message: "Your credentials are invalid"})
+  end
+
+  it "gives error when left blank" do
+
+    params = {
+      "email": "",
+      "password": ""
+    }
+
+    post "/api/v0/sessions", params: params.to_json, headers: {'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json'}
+
+    expect(response.status).to eq(401)
+    
+    message = JSON.parse(response.body, symbolize_names: true)
+    expect(message).to eq({message: "Your credentials are invalid"})
   end
 end
